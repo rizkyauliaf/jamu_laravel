1a4cb7967bde1a2702d9695f8fe6fc72f3181fdd
rizkyauliaf <rizkyauliaf7@gmail.com>
2022-09-08 21:16:58 +0700

    Menambah Konten 2

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,29 +1,29 @@
 <header id="header" class="fixed-top">
-        <div class="container d-flex align-items-center justify-content-between">
+    <div class="container d-flex align-items-center justify-content-between">
 
-            <h1 class="logo"><a href="index.html">JAMU KITA</a></h1>
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <h1 class="logo"><a href="index.html">JAMU KITA</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
-                    <li class="dropdown"><a href="#"><span>Tentang</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="#">Sejarah</a></li>
-                            <li><a href="#">Proses Produksi</a></li>
-                            <li><a href="#">Mitra Kami</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="nav-link scrollto" href="#services">Produk Inovasi</a></li>
-                    <li><a class="nav-link scrollto " href="#portfolio">Berita Terkini</a></li>
-                    <li><a class="nav-link scrollto" href="#team">Tim</a></li>
-                    
-                    <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
-                    <!-- <li><a class="getstarted scrollto" href="#about">Login</a></li> -->
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+                <li class="dropdown"><a href="#"><span>Tentang</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a class="nav-link scrollto" href="about">Sejarah</a></li>
+                        <li><a class="nav-link scrollto" href="services">Proses Produksi</a></li>
+                        <li><a href="#">Mitra Kami</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#portfolio">Produk Inovasi</a></li>
+                <li><a class="nav-link scrollto " href="#featured-services">Berita Terkini</a></li>
+                <li><a class="nav-link scrollto" href="#team">Tim</a></li>
 
-        </div>
-    </header><!-- End Header -->
+                <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+                <!-- <li><a class="getstarted scrollto" href="#about">Login</a></li> -->
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
