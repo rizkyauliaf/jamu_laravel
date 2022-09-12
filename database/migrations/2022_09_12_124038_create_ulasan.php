4d2697d21c4637723704234ccf7b3853d9102dfe
viabrinsa <viabrinsa.mailbox@gmail.com>
2022-09-12 19:49:40 +0700

    menambah tabel ulasan

---
+++ database/migrations/2022_09_12_124038_create_ulasan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ulasan', function (Blueprint $table) {
+            $table->increments('id_ulasan');
+            $table->string('nama_pengirim');
+            $table->text('ulasan');
+            $table->string('pekerjaan');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ulasan');
+    }
+};
