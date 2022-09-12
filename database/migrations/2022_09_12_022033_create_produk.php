5b1a4d5856a1dce87ad8b847f1e75562e87d2ca9
rizkyauliaf <rizkyauliaf7@gmail.com>
2022-09-12 09:55:25 +0700

    Menyambungkan database

---
+++ database/migrations/2022_09_12_022033_create_produk.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_produk');
+            $table->float('harga_produk');
+            $table->text('deskripsi_produk');
+            $table->enum('jenis_produk', ['Bubuk', 'Cair']);
+            $table->string('gambar_produk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+};
